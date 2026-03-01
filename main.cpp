2f637eb4980b306d8b5c61c0f9c69b3f82cfe15f
Valentine Chibinyov <chibinyov@rambler.ru>
2026-03-01 21:10:08 +0300

    wide char usage

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <fstream>  // Для работы с файлами
 #include <string>
+#include <locale>
 
 int main() {
+    std::wcout.imbue(std::locale(""));
     // 1. Открываем файл
-    std::ifstream file("./file.txt");
-    std::ofstream outFile("./output.txt");
+    std::wifstream file("./file.txt");
+    std::wofstream outFile("./output.txt");
     // std::ofstream file2("./example.txt", std::ios::app);
-     std::ofstream file2("./example.txt");
+     std::wofstream file2("./example.txt");
 
     int sdvig = 8;
 
@@ -16,23 +18,23 @@ int main() {
         std::cerr << "Ошибка: файл не найден!" << std::endl;
         return 1;
     } else if (file.is_open() && outFile.is_open() && file2.is_open()) {
-        char ch;
+        wchar_t ch;
         // Считываем по одному символу в переменную ch
         while (file.get(ch)) {            
             int code = static_cast<int>(ch) + sdvig; 
-            ch = static_cast<char>(code);
+            ch = static_cast<wchar_t>(code);
             outFile << ch;
 
         }
     }
     outFile.close();
-    std::ifstream outFile_2("./output.txt");
+    std::wifstream outFile_2("./output.txt");
 
     if(outFile_2.is_open() && file2.is_open()){
-        char ch;
+        wchar_t ch;
         while(outFile_2.get(ch)){
         int code = static_cast<int>(ch) - sdvig;
-        ch = static_cast<char>(code);
+        ch = static_cast<wchar_t>(code);
         file2 << ch;
     }
     }
